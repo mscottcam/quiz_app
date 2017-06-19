fb766a6b66e77ddb731921dac79d1988ce57f0b6
mscottcam <mscottcam@gmail.com>
2017-06-19 11:13:48 -0500

    initial setup

---
+++ app.js
@@ -0,0 +1,48 @@
+const appState = {
+  //view: initial state of intro page
+
+  // correct?: true or false
+}
+
+
+// make html skeleton
+// design state
+
+  // figure out state layout
+    // views -- intro page, questions, results page
+    // order of questions
+
+  // current score variable
+
+  // current question variable
+
+  // final score variable
+
+
+// write state mod functions
+
+  // changeView function
+
+  // correct or not function
+
+
+// write render function
+
+  //view toggle
+    // question render fn
+    // final page render
+
+
+
+// write event listeners
+
+  // intro views
+    //start button click
+
+  //questions
+    //submit button
+    //next button
+    //radio selector click
+
+  // results page
+    //restart click
